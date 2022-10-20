304843842fbf8fcc3ba8bfd2d228d7eabb5da142
simphiwe-nkabinde <simphiwe.nkabinde@dadevs.co.za>
2022-10-20 08:11:31 +0200

    create components

--- modules/components.js
+++ modules/components.js
@@ -62,18 +62,21 @@ const TextForm = {
 const TextAreaForm = {
     icon: `
         <i class="bi bi-pencil-square"></i>
-        <small>Text Area Form</small>
+        <small>Text Form</small>
         `,
     content: `
         <div class='small mb-3 mx-2'>
-            <label contenteditable for="Textarea" class="form-label fw-bold">Text Area form</label>
+            <label contenteditable for="Textarea" class="form-label fw-bold">Text form</label>
             <textarea class="form-control" id="Textarea" rows="3"></textarea>
         </div>
-            `  
+            `
 }
 
 const Header = {
-    icon: `header`,
+    icon: `
+        <i class="bi bi-window-sidebar"></i>
+        <small>Header</small>
+        `,
     content: `
         <div class="bg-dark text-light p-2 mb-3">
             <h1 contenteditable class="dispay-4 small">Header</h1>
@@ -81,5 +84,69 @@ const Header = {
         `
 }
 
+const Video = {
+    icon: '<i class="bi bi-camera-video"></i>',
+    content: `
+            <video controls width="250">
+                <source src="/media/cc0-videos/flower.webm" type="video/webm">
+                <source src="/media/cc0-videos/flower.mp4" type="video/mp4">
+                Sorry, your browser doesn't support embedded videos.
+            </video>
+            `
+}
 
-export { Button, EmailForm, RadioForm, TextForm, TextAreaForm, Header }
+const Hr = {
+    icon: `
+            <i class="bi bi-hr"></i>
+            <small>Divider</small>
+            `,
+    content: `<hr/ class='mx-2'>`
+}
+
+const Heading1 = {
+    icon: `<i class="bi bi-type-h1"></i>`,
+    content: `<h1 contenteditable>Heading 1</h1>`
+}
+
+const Heading2 = {
+    icon: `<i class="bi bi-type-h2"></i>`,
+    content: `<h2 contenteditable>Heading 2</h2>`
+}
+
+const Heading3 = {
+    icon: `<i class="bi bi-type-h3"></i>`,
+    content: `<h3 contenteditable>Heading 1</h3>`
+}
+
+const Text = {
+    icon: '<i class="bi bi-fonts"></i>',
+    content: `<div contenteditable><p>Lorem Ipsum is
+                     simply dummy text of the printing and typesetting industry. 
+                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                     when an unknown printer took a galley of type and scrambled it to make a 
+                     type specimen book.
+              </p><div>`
+  }
+
+const Image = {
+    icon: '<i class="bi bi-image"></i>',
+    content: '<img src="https://picsum.photos/100"/>'
+  }
+
+
+
+export {
+    Button,
+    EmailForm,
+    RadioForm,
+    TextForm,
+    TextAreaForm,
+    Header,
+    Video,
+    Hr,
+    Heading1,
+    Heading2,
+    Heading3,
+    Text,
+    Image
+}
