1a68ab0cce560aeb593539b51318436659e6e0b8
Israel Kayaba <Israel@thedigitalacademy.co.za>
2022-11-28 07:52:05 +0200

    ecommerce start

--- modules/exportableProject.class.js
+++ modules/exportableProject.class.js
@@ -31,7 +31,7 @@ class ExportableProject {
         //remove all contenteditable element attributes
         const finalRootHtml = rootInnerHtml.replace(/contenteditable=""/g, "");
 
-        const HtmlBlob =`
+        const HtmlBlob = `
         <!DOCTYPE html>
         <html lang="en">
           <head>
@@ -40,18 +40,23 @@ class ExportableProject {
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>HTML 5 Boilerplate</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
             <link rel="stylesheet" type="text/css" href="./style.css">
             <script src="./scripts/ayobastub.js"></script>
             <script src="./scripts/microapp.js"></script>
           </head>
-          <body>
+          <body class="m-0 p-0 d-flex justify-content-center">
             ${finalRootHtml}
             <script src="./scripts/script.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
           </body>
         </html>
-                        `
+        `
         const file = new Blob(
             [HtmlBlob],
             { type: 'text/html' }
@@ -93,10 +98,10 @@ class ExportableProject {
         zip.file("scripts/microapp.js", MicroAppJs);
         zip.file("scripts/ayobastub.js", AyobaStubJs);
 
-        zip.generateAsync({type:"blob"})
-        .then(function (blob) {
-            saveAs(blob, "studio_microapp.zip");
-        });
+        zip.generateAsync({ type: "blob" })
+            .then(function (blob) {
+                saveAs(blob, "studio_microapp.zip");
+            });
     }
 }
 
--- modules/templateComponents.js
+++ modules/templateComponents.js
@@ -1,11 +1,18 @@
 import ELEMENTS from "./components.js";
+import eCommerce from "./eCommerce/eCommerce.js";
 
 const { EmailForm, RadioForm2, RadioForm, TextForm, RadioForm3, PhoneForm, TextAreaForm, Image, Button } = ELEMENTS;
+const { Main } = eCommerce;
+
 
 const TEMPELEMENTS = {
     Template1: {
         icon: `<p>Survey Template</p>`,
         elements: [EmailForm, RadioForm, RadioForm2, TextForm, RadioForm3, PhoneForm, TextAreaForm, Image, Button],
+    },
+    Template2: {
+        icon: `<p>eCommerce Template</p>`,
+        elements: [Main],
     }
 }
 
