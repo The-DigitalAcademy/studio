99435329fdf8ba8f9d246955f903d543e1309c6f
Israel Kayaba <Israel@thedigitalacademy.co.za>
2022-11-30 11:12:52 +0200

    Last touch

--- modules/exportableProject.class.js
+++ modules/exportableProject.class.js
@@ -50,8 +50,8 @@ class ExportableProject {
             <script src="./scripts/ayobastub.js"></script>
             <script src="./scripts/microapp.js"></script>
           </head>
-          <body class="m-0 p-0 d-flex justify-content-center">
-            ${finalRootHtml}
+          <body">
+            ${finalRootHtml} 
             <script src="./scripts/script.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
           </body>
