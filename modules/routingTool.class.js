417ab7a1c853e9d4b919217d95a473c5d0e69f42
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2023-01-03 00:35:52 +0200

    routing tool

--- modules/routingTool.class.js
+++ modules/routingTool.class.js
@@ -27,15 +27,35 @@ class RoutingTool {
         }
         routesDropdown.innerHTML = ""
         this.projectData.pages.forEach(page => {
-            const option = document.createElement('option');
-            option.value = page.filename;
-            option.innerText = page.name;
-            routesDropdown.append(option);
+
+            const isChecked = targetComponent.attributes.href == `./${page.filename}.html` ? true : false
+
+            const radioContainer = document.createElement('div');
+            radioContainer.className = "form-check";
+
+            const radioInput = document.createElement('input')
+            radioInput.type = "radio"
+            radioInput.className = "form-check-input"
+            radioInput.checked = isChecked
+            radioInput.name = "route"
+            radioInput.value = page.filename
+            radioInput.id = page.filename
+            radioInput.onclick = () => this.handleRouteSelect()
+
+            const radioLabel = document.createElement('label');
+            radioLabel.className = "form-check-label small"
+            radioLabel.for = page.filename 
+            radioLabel.innerText = page.name
+
+            radioContainer.append(radioInput, radioLabel)
+
+            routesDropdown.append(radioContainer)
         })
     }
-    handleRouteSelect(e) {
+    handleRouteSelect() {
     const { page, component } = getHashData();
-    const selectedPage = e.currentTarget.value;
+
+    const selectedPage = document.querySelector('input[name="route"]:checked').value
 
     const targetComponent = findComponentById(component, this.projectData.pages[page].components);
     targetComponent.attributes.href = './'+selectedPage+'.html';
